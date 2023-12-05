8f733d46e73627877cfe99893c5dca025e13b021
Nitzan Cohen <nitzancohen@hotmail.com>
2023-12-05 12:18:11 +0200

    Upload initial learning project

---
+++ src/app/page.js
@@ -0,0 +1,13 @@
+import styles from './page.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <ul>
+        <li>Product 1</li>
+        <li>Product 2</li>
+        <li>Product 3</li>
+      </ul>
+    </main>
+  )
+}
