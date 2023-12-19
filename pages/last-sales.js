03ccc33cbac35338133b6fdc4e0c814ef397dd3c
Nitzan Cohen <nitzancohen@hotmail.com>
2023-12-19 12:51:04 +0200

    Conclusion of section 6

--- pages/last-sales.js
+++ pages/last-sales.js
@@ -4,12 +4,38 @@
  */
 
 import { useEffect, useState } from "react";
-
+// import useSWR from "swr";
 
 
 function LastSalesPage() {
     const [sales, setSales] = useState();
     const [isLoading, setIsLoading] = useState(false);
+
+    // const { data, error } = useSWR('https://nextjs-learning-b3b77-default-rtdb.firebaseio.com/sales.json');
+
+    // useEffect(() => {
+    //     if (data) {
+    //         const transformedSales = [];
+    //         for (const key in data) {
+    //             transformedSales.push({
+    //                 id: key,
+    //                 username: data[key].username,
+    //                 volume: data[key].volume
+    //             })
+    //         }
+    //         setSales(transformedSales)
+    //     }
+
+    // }, [data]);
+    
+    // if (error) {
+    //     return <p>Faild to load.</p>
+    // }
+
+    // if (!data || !sales) {
+    //     return <p>Loading...</p>
+    // }
+
 
     useEffect(() => {
         setIsLoading(true);
@@ -33,22 +59,20 @@ function LastSalesPage() {
         })
     }, [])
 
+    if (!sales) {
+        return <p>No data yet.</p>
+    }
+
     if (isLoading) {
         return <p>Loading...</p>
     }
-
-    if (!sales) {
-        return <p>No data yet</p>
-    }
-
-    console.log(sales);
 
     return (
         <ul>
             {
                 sales.map(sale => (
                     <li key={sale.id}>
-                        {sale.username} - {sale.volume}
+                        {sale.username} - {sale.volume}$
                     </li>
             ))}
         </ul>
