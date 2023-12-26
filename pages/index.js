b03518ed92d6f126559736f276633cdab020c5d0
Nitzan Cohen <nitzancohen@hotmail.com>
2023-12-26 16:14:56 +0200

    Section9 - API routes

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,10 @@
-import path from 'path';
-import fs from 'fs/promises'; // Using file system to manage read files.
-import Link from 'next/link';
+import path from "path";
+import fs from "fs/promises"; // Using file system to manage read files.
+import Link from "next/link";
 
 /**
- * * SSG - Static Site Generation page component. 
- * * With the function getStaticProps in definition for this page. 
+ * * SSG - Static Site Generation page component.
+ * * With the function getStaticProps in definition for this page.
  */
 
 function HomePage(props) {
@@ -12,29 +12,9 @@ function HomePage(props) {
 
   return (
     <main>
-      <ul>
-        {
-          products.map(product => (
-            <li key={product.id}>
-              <Link href={`/products/${product.id}`}>{product.title}</Link>
-            </li>
-          ))
-        }
-      </ul>
+      <h1>Home Pgae</h1>
     </main>
-  )
-}
-
-export async function getStaticProps() {
-  console.log('(Re-)Generation...');
-  const filePath = path.join(process.cwd(), 'data', 'dummy-backend.json'); // This gave the absoulte path to data file.
-  const jsonData = await fs.readFile(filePath);
-  const data = JSON.parse(jsonData);
-
-  return {
-    props: { products: data.products },
-    revalidate: 15
-  }
+  );
 }
 
 export default HomePage;
