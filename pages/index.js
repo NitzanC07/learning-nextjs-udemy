75b026a100c5cfc95217963b7dc4eab07b7227c5
Nitzan Cohen <nitzancohen@hotmail.com>
2023-12-27 19:53:45 +0200

    Lesson: API requests POST and GET

--- pages/index.js
+++ pages/index.js
@@ -1,45 +1,52 @@
 import path from "path";
 import Link from "next/link";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 /**
- * * API Routes
+ * * Section 9 - API Routes
+ * * Create POST and GET requests with API and write and read data from a file.
  */
 
 function HomePage(props) {
   const { products } = props;
+
+  const [feedbackItems, setFeedbackItems] = useState([]);
 
   const emailInputRef = useRef();
   const feedbackInputRef = useRef();
 
   function submitFormHandler(event) {
     /**
-     * ! באופן אוטומטי, ברגע שלוחצים על כפתור שליחת הטופס, אז העמוד נטען מחדש.
-     * ! כדי למנוע את הטעינה מחדש, אנחנו צריכים להוסיף את שורת הקוד הבאה.
+     * ! הפונקציה הזאת מופעלת לאחר שליחת המידע שמוזן לטופס.
+     * ! היא אוספת את הנתונים בשלב הראשון (1), מארגנת אותם במבנה מסודר (2),
+     * ! שולחת בקשת שרת עם הפרמטרים הרלוונטיים (3), ומקבלת תגובה חזרה (4)
      */
     event.preventDefault();
 
-    /**
-     * ! בשלב זה הפונקציה מבצעת איסוף נתונים מתוך הטופס למשתנים מתאימים.
-     */
-    const enteredEmailUser = emailInputRef.current.value;
+    const enteredEmailUser = emailInputRef.current.value; // *1
     const enteredFeedbackUser = feedbackInputRef.current.value;
 
-    /**
-     * ! בשלב זה הפונקציה תארוז את כל הנתונים שהיא אספה ותשלח אותם לשרת.
-     * * {email: 'someone@somewhat.com', text: 'Some feedback text.'}
-     */
-    console.log(enteredEmailUser, enteredFeedbackUser);
+    const dataForm = { email: enteredEmailUser, text: enteredFeedbackUser }; // *2
+
     fetch("/api/feedback", {
-      method: 'POST',
-      body: JSON.stringify({
-        email: enteredEmailUser,
-        text: enteredFeedbackUser,
-      }),
+      // *3
+      method: "POST",
+      body: JSON.stringify(dataForm),
       headers: { "Content-Type": "application/json" },
     })
+      .then((response) => response.json()) // *4
+      .then((data) => console.log(data)); // *4
+  }
+
+  function loadFeedbackHandler() {
+    /**
+     * ! הפונקציה מופעלת כאשר יש בקשה מצד הלקוח לקרוא את הנתונים.
+     * ! בעת הרצת הפונקציה נשלחת בקשת קריאה, שהיא ברירת המחדל, לכן אין הכרח
+     * ! לציין זאת בגוף הבקשה, אלא רק לציין את הנתיב של הבקשה.
+     */
+    fetch("/api/feedback")
       .then((response) => response.json())
-      .then((data) => console.log(data));
+      .then((data) => setFeedbackItems(data.feedback))
   }
 
   return (
@@ -56,6 +63,14 @@ function HomePage(props) {
         </div>
         <button type="submit">Send feedback</button>
       </form>
+      <button onClick={loadFeedbackHandler}>Load feedback</button>
+      <ul>
+        {
+          feedbackItems.map(item => (
+            <li key={item.id}>{item.text}</li>
+          ))
+        }
+      </ul>
     </main>
   );
 }
