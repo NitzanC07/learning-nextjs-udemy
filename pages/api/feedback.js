6b1aba2f08147da24806ac6970f8bbd689a4d23d
Nitzan Cohen <nitzancohen@hotmail.com>
2023-12-27 13:39:10 +0200

    Lesson: Using API method and write to a file

--- pages/api/feedback.js
+++ pages/api/feedback.js
@@ -1,5 +1,40 @@
+import fs from "fs";
+import path from "path";
+
 function handler(req, res) {
-    res.status(200).json({message: "This works!"});
+  if (req.method === "POST") {
+    /**
+     * ! בשלב הראשון הפונקציה בודקת את שיטת הבקשה.
+     * * POST, GET or something else.
+     * ! בשלב הבא אחרי שהיא ווידאה זאת הפונקציה אוספת את המידע ושומרת במשתנים.
+     */
+    const email = req.body.email;
+    const feedbackText = req.body.text;
+
+    /**
+     * ! בשלב הבא הפונקציה אורזת את המידע שהתקבל בתוך אובייקט מסודר.
+     * ! בשלב זה גם רצוי להוסיף למידע איזה שהוא סימן ייחודי כדי להבדיל אותו משאר הפרטים הקיימים במאגר.
+     * * id
+     */
+    const newFeedback = {
+      id: new Date().toString(),
+      email: email,
+      text: feedbackText,
+    };
+
+    /**
+     * ! בשלב זה הפונקציה מאחסנת את המידע שהתקבל במאגר נתונים או בקובץ.
+     * ! הדוגמא שלהלן שומרת את המידע בקובץ נפרד בתוך הפרויקט.
+     */
+    const filePath = path.join(process.cwd(), "data", "feedback.json"); // יוצר נתיב לקובץ שאליו אנחנו רוצים לכתוב
+    const fileData = fs.readFileSync(filePath); // קורא את הקובץ שאליו אנחנו רוצים להוסיף מידע חדש.
+    const data = JSON.parse(fileData); // הופך את המידע שהתקבל לאובייקט מסוג JSON.
+    data.push(newFeedback); // מוסיף את המידע החדש שהתקבל.
+    fs.writeFileSync(filePath, JSON.stringify(data)); // כותב מחדש לתוך הקובץ עם המידע המעודכן.
+    res.status(201).json({ message: "Success", feedback: newFeedback }); // שולח תגובה שהתהליך הצליח.
+  } else {
+    res.status(200).json({ message: "This works!" });
+  }
 }
 
 export default handler;
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import path from "path";
 import Link from "next/link";
+import { useRef } from "react";
 
 /**
  * * API Routes
@@ -8,9 +9,53 @@ import Link from "next/link";
 function HomePage(props) {
   const { products } = props;
 
+  const emailInputRef = useRef();
+  const feedbackInputRef = useRef();
+
+  function submitFormHandler(event) {
+    /**
+     * ! באופן אוטומטי, ברגע שלוחצים על כפתור שליחת הטופס, אז העמוד נטען מחדש.
+     * ! כדי למנוע את הטעינה מחדש, אנחנו צריכים להוסיף את שורת הקוד הבאה.
+     */
+    event.preventDefault();
+
+    /**
+     * ! בשלב זה הפונקציה מבצעת איסוף נתונים מתוך הטופס למשתנים מתאימים.
+     */
+    const enteredEmailUser = emailInputRef.current.value;
+    const enteredFeedbackUser = feedbackInputRef.current.value;
+
+    /**
+     * ! בשלב זה הפונקציה תארוז את כל הנתונים שהיא אספה ותשלח אותם לשרת.
+     * * {email: 'someone@somewhat.com', text: 'Some feedback text.'}
+     */
+    console.log(enteredEmailUser, enteredFeedbackUser);
+    fetch("/api/feedback", {
+      method: 'POST',
+      body: JSON.stringify({
+        email: enteredEmailUser,
+        text: enteredFeedbackUser,
+      }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
   return (
     <main>
       <h1>Home Pgae</h1>
+      <form onSubmit={submitFormHandler}>
+        <div>
+          <label htmlFor="email">Your Email address</label>
+          <input type="email" id="email" ref={emailInputRef} />
+        </div>
+        <div>
+          <label htmlFor="feedback">Your feedback</label>
+          <textarea id="feedback" rows="5" ref={feedbackInputRef}></textarea>
+        </div>
+        <button type="submit">Send feedback</button>
+      </form>
     </main>
   );
 }
